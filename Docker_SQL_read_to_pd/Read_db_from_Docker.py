deaefcde875e4baf0a1839f3c06cbd86796f5702
samblyth995 <109956274+samblyth995@users.noreply.github.com>
2024-06-23 20:22:30 +0100

    fix issue

---
+++ Docker_SQL_read_to_pd/Read_db_from_Docker.py
@@ -0,0 +1,3 @@
+##I created a volume in docker and mounted a mongodb image. I then coped a csv data set to the volume and used sql lite to create a database from it
+#this script now acceses that database
+
